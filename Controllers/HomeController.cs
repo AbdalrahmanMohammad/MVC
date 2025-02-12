654b715f33f80b1603235057ee8d35ec7a59ec78
Abdalrahman Mohammad <abdalrahman.s.mohammad@gmail.com>
2025-02-12 23:46:42 +0200

    add Geo location

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
+using TeddySmith.helpers;
+using TeddySmith.Interfaces;
 using TeddySmith.Models;
+using TeddySmith.Repository;
+using TeddySmith.ViewModels;
 
 namespace TeddySmith.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository)
         {
             _logger = logger;
+            this.clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token=f99d50e18d873c";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if (homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception)
+            {
+                homeViewModel.Clubs = null;
+            }
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TeddySmith.Models;
+
+namespace TeddySmith.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club>? Clubs { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+    }
+}
